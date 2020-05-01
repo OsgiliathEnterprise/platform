f9c19238a05b2b174a05a5ec0a7fb4a3ec1f0e4e
Charlie Mordant <cmordant1@gmail.com>
2020-05-01 16:42:44 +0200

    introduce directory repository

--- ansible/roles/ansible-directory/molecule/default/tests/test_default.py
+++
@@ -1,7 +0,0 @@
-"""Role testing files using testinfra."""
-
-
-def test_389_ds_is_installed(host):
-    command = """rpm -qa | grep 389-ds"""
-    cmd = host.run(command)
-    assert '389-ds' in cmd.stdout

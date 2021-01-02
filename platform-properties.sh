db98514369af0e123d8d87e7e16faff344f4614e
Charlie Mordant <cmordant1@gmail.com>
2021-01-02 23:34:41 +0100

    up to date platform

--- platform-properties.sh
+++ platform-properties.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 distro=fedora
-distroshortversion="$distro-31"
-distroversion="$distro-31-x86_64" # 30 not compatible with Vagrant 2.2.5
+distroshortversion="$distro-33"
+distroversion="$distroshortversion-x86_64" # 30 not compatible with Vagrant 2.2.5

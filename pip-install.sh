62e695b22804d019b7e72d16aaf5a881189d0be3
Continuous integration <ci@osgiliath.net>
2024-03-03 22:47:02 +0000

    Site checkin for project Osgiliath Platform

--- pip-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip freeze | xargs pip uninstall -y
-pip install ansible-compat==0.5.0 ansible-lint six yamllint pytest-testinfra molecule molecule-vagrant= ansible netaddr python-vagrant flake8 jmespath molecule-docker
--- platform-properties.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-distro=fedora
-distroshortversion="$distro-35"
-distroversion="$distroshortversion-x86_64" # 30 not compatible with Vagrant 2.2.5
